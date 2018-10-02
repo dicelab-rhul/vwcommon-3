624b6e7adc185b68a8ed7e4de438ada0c644dc89
cloudstrife9999 <Emanuele.Uliana.2016@live.rhul.ac.uk>
2018-10-02 17:31:36 +0100

    Minor.

--- src/main/java/uk/ac/rhul/cs/dice/vacuumworld/vwcommon/VWAbstractMessage.java
+++ src/main/java/uk/ac/rhul/cs/dice/vacuumworld/vwcommon/VWAbstractMessage.java
@@ -5,7 +5,7 @@ import org.json.JSONObject;
 public abstract class VWAbstractMessage implements VWMessage {
     private static final long serialVersionUID = -3789213671006332038L;
     private VWMessageCodes code;
-    private transient JSONObject content;
+    private JSONObject content;
     
     public VWAbstractMessage(VWMessageCodes code, JSONObject content) {
 	this.code = code;
