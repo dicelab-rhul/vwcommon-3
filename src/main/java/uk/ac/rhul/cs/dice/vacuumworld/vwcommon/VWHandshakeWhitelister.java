81cecb71f12e95b6a68f16d93b1f2f941e5f4938
cloudstrife9999 <Emanuele.Uliana.2016@live.rhul.ac.uk>
2018-11-14 20:08:09 +0000

    Refined the whitelister.

--- src/main/java/uk/ac/rhul/cs/dice/vacuumworld/vwcommon/VWHandshakeWhitelister.java
+++ src/main/java/uk/ac/rhul/cs/dice/vacuumworld/vwcommon/VWHandshakeWhitelister.java
@@ -3,19 +3,25 @@ package uk.ac.rhul.cs.dice.vacuumworld.vwcommon;
 import java.io.Serializable;
 
 import org.apache.commons.io.serialization.ValidatingObjectInputStream;
+import org.cloudstrife9999.logutilities.LogUtils;
 
 public class VWHandshakeWhitelister {
-    
+
     private VWHandshakeWhitelister() {}
-    
+
     public static void whitelistHandshakeClasses(ValidatingObjectInputStream is) {
-	is.accept(VacuumWorldMessage.class);
-	is.accept(VWAbstractMessage.class);
-	is.accept(VWMessage.class);
-	is.accept(VWMessageCodes.class);
-	is.accept(Enum.class);
-	is.accept(Serializable.class);
-	is.accept(String.class);
-	is.accept(Long.class);
+	if (is == null) {
+	    LogUtils.log("The ValidatingInputStream passed as parameter is null. Cannot whitelist anything...");
+	}
+	else {
+	    is.accept(VacuumWorldMessage.class);
+	    is.accept(VWAbstractMessage.class);
+	    is.accept(VWMessage.class);
+	    is.accept(VWMessageCodes.class);
+	    is.accept(Enum.class);
+	    is.accept(Serializable.class);
+	    is.accept(String.class);
+	    is.accept(Long.class);
+	}
     }
 }
